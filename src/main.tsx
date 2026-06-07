579d4e7bb3e8fde5229555ce0b3b561ea1be1571
Greg-hms <greg@hms.com>
2026-06-08 01:18:13 +0700

    fix: add ReactDOM.createRoot to mount the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-export default function Root() {
-  return (
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
